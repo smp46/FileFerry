f2e36a867562ada8f239b6fc2b4f04d5caf3d474
smp46 <me@smp46.me>
2025-06-02 13:56:23 +1000

    restyled send window

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default {
   build: {
-    target: "es2022",
+    target: 'es2022',
   },
   optimizeDeps: {
-    esbuildOptions: { target: "es2022", supported: { bigint: true } },
+    esbuildOptions: { target: 'es2022', supported: { bigint: true } },
   },
   server: {
     open: true,
   },
   plugins: [tailwindcss()],
   define: {
-    global: "globalThis",
+    global: 'globalThis',
   },
 };
