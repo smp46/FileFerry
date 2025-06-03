727cf9d62bda0845aaa739582fe0167ee65b0d9c
smp46 <me@smp46.me>
2025-06-03 21:46:44 +1000

    Switching to repo structure that allows for staging branch and deployment

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,24 @@
+import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default {
-  build: {
-    target: 'es2022',
-  },
-  optimizeDeps: {
-    esbuildOptions: { target: 'es2022', supported: { bigint: true } },
-  },
-  server: {
-    open: true,
-  },
-  plugins: [tailwindcss()],
-  define: {
-    global: 'globalThis',
-  },
-};
+export default defineConfig(({ mode }) => {
+  const isStaging = mode === 'staging';
+
+  return {
+    build: {
+      target: 'es2022',
+    },
+    optimizeDeps: {
+      esbuildOptions: { target: 'es2022', supported: { bigint: true } },
+    },
+    server: {
+      open: true,
+    },
+    plugins: [tailwindcss()],
+    define: {
+      global: 'globalThis',
+    },
+
+    base: isStaging ? '/staging/' : '/',
+  };
+});
