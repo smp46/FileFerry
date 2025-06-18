7c8b647cb45585fca1cf5708bebe71f8a6bfa6f5
smp46 <me@smp46.me>
2025-06-18 20:55:45 +1000

    Relay now uses auto-tls to acquire cert

--- backend/relay-server/relay.js
+++ backend/relay-server/relay.js
@@ -5,19 +5,21 @@ import {
   circuitRelayServer,
 } from '@libp2p/circuit-relay-v2';
 import { dcutr } from '@libp2p/dcutr';
-import { identify } from '@libp2p/identify';
+import { identify, identifyPush } from '@libp2p/identify';
+import { tls } from '@libp2p/tls';
+import { loadOrCreateSelfKey } from '@libp2p/config';
+import { keychain } from '@libp2p/keychain';
 import { autoNAT } from '@libp2p/autonat';
+import { autoTLS } from '@ipshipyard/libp2p-auto-tls';
 import { tcp } from '@libp2p/tcp';
 import { webSockets } from '@libp2p/websockets';
 import { webRTC, webRTCDirect } from '@libp2p/webrtc';
-import { mplex } from '@libp2p/mplex';
+import { WebSocketsSecure } from '@multiformats/multiaddr-matcher';
+import { LevelDatastore } from 'datastore-level';
 import { ping } from '@libp2p/ping';
 import { createLibp2p } from 'libp2p';
-import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string';
-import { privateKeyFromProtobuf } from '@libp2p/crypto/keys';
 
 const ANNOUNCE_HOST = process.env.ANNOUNCE_HOST || '';
-const ANNOUNCE_PORT = parseInt(process.env.ANNOUNCE_PORT || 443);
 const LISTEN_HOST = process.env.LISTEN_HOST || '0.0.0.0';
 const LISTEN_PORT = parseInt(process.env.LISTEN_PORT || 41337);
 const MAX_RESERVATIONS = parseInt(process.env.MAX_RESERVATIONS || '50', 10);
@@ -25,38 +27,6 @@ const RESERVATION_TTL = parseInt(
   process.env.RESERVATION_TTL || 60 * 60 * 1000,
   10,
 );
-
-async function getPrivateKeyObjectFromEnv() {
-  const privKeyBase64 = process.env.PRIV_KEY_BASE64;
-
-  if (!privKeyBase64) {
-    console.error(`Error: Environment variable is not set.`);
-    return null;
-  }
-
-  try {
-    const protobufEncodedPrivKeyBytes = uint8ArrayFromString(
-      privKeyBase64,
-      'base64pad',
-    );
-
-    const privateKeyObject = privateKeyFromProtobuf(
-      protobufEncodedPrivKeyBytes,
-    );
-
-    console.log(
-      'Successfully created PrivateKey object from protobuf. Type:',
-      privateKeyObject.type,
-    );
-    return privateKeyObject;
-  } catch (error) {
-    console.error(
-      'Error creating PrivateKey object from protobuf string:',
-      error,
-    );
-    return null;
-  }
-}
 
 async function main() {
   let serverNode;
@@ -78,10 +48,14 @@ async function main() {
   process.on('SIGTERM', () => shutdown('SIGTERM'));
 
   try {
-    const loadedPrivateKey = await getPrivateKeyObjectFromEnv();
+    const datastore = new LevelDatastore('/usr/src/app/db/db');
+    await datastore.open();
+
+    const privateKey = await loadOrCreateSelfKey(datastore);
 
     serverNode = await createLibp2p({
-      privateKey: loadedPrivateKey,
+      datastore: datastore,
+      privateKey: privateKey,
       addresses: {
         listen: [
           `/ip4/${LISTEN_HOST}/tcp/${LISTEN_PORT}/`,
@@ -89,7 +63,12 @@ async function main() {
           `/ip4/${LISTEN_HOST}/udp/${LISTEN_PORT + 2}/webrtc-direct`,
           `/p2p-circuit`,
         ],
-        announce: [`/dns4/${ANNOUNCE_HOST}/tcp/${ANNOUNCE_PORT}/wss`],
+        announce: [
+          `/ip4/${LISTEN_HOST}/tcp/${LISTEN_PORT}/`,
+          `/ip4/${LISTEN_HOST}/tcp/${LISTEN_PORT + 1}/ws`,
+          `/ip4/${LISTEN_HOST}/udp/${LISTEN_PORT + 2}/webrtc-direct`,
+          `/p2p-circuit`,
+        ],
       },
       transports: [
         circuitRelayTransport(),
@@ -98,11 +77,16 @@ async function main() {
         webRTCDirect(),
         webSockets(),
       ],
-      connectionEncrypters: [noise()],
-      streamMuxers: [yamux(), mplex()],
+      connectionEncrypters: [noise(), tls()],
+      streamMuxers: [yamux()],
       services: {
         identify: identify(),
+        identifyPush: identifyPush(),
+        keychain: keychain(),
         ping: ping(),
+        autoTLS: autoTLS({
+          autoConfirmAddress: true,
+        }),
         dcutr: dcutr(),
         autoNAT: autoNAT(),
         relay: circuitRelayServer({
@@ -112,6 +96,27 @@ async function main() {
           },
         }),
       },
+    });
+
+    serverNode.addEventListener('certificate:provision', () => {
+      console.info('A TLS certificate was provisioned');
+
+      const interval = setInterval(() => {
+        const mas = serverNode
+          .getMultiaddrs()
+          .filter(
+            (ma) =>
+              WebSocketsSecure.exactMatch(ma) &&
+              ma.toString().includes('/sni/'),
+          )
+          .map((ma) => ma.toString());
+
+        if (mas.length > 0) {
+          console.info('addresses:');
+          console.info(mas.join('\n'));
+          clearInterval(interval);
+        }
+      }, 1_000);
     });
 
     console.info(`Relay node ${serverNode.peerId.toString()} started.`);
